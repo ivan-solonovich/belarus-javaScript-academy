9ff77851f356019cd73904fd6d9bccf822d1d1e7
ivan-solonovich <solonovichivan1978@gmail.com>
2023-07-13 23:08:02 +0300

    first

--- src/components/main-page/MainReason.vue
+++ src/components/main-page/MainReason.vue
@@ -21,7 +21,53 @@
       Запатрабаваны на рынку, попыт на JS распрацоўшчыкаў доўгія гады перавышае прапанову. У топе албо на першай пазіціі, аплшніх 15 год.
     </div>
 
+
   </div>
+  <div class="container mx-auto columns-1 md:columns-2xl lg:columns-2xl gap-x-5 gap-y-5 pt-2">
+    <div
+        class="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700  md:flex-row">
+      <img style="max-width: 200px"
+          class="h-96 w-96 rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src="../../assets/img/young-coworkers-formal-clothes.jpg"
+          alt="" />
+      <div class="flex flex-col justify-start p-6">
+        <h5
+            class="mb-2 text-xl font-medium text-red-700 dark:text-neutral-50">
+          Вопыт ментора у распрацоўцы больш, чым за 20 гадоў.
+        </h5>
+        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          Усё сёння можна знайсці ў сетцы? - гэта дакладна. Аднак пошук можа заняць занадта шмат часу і наогул
+          не ўвянчацца поспехам. Вопытны ментар, разумее што шукаць і дзе шукаць, а часцей за ўсё,
+          проста, ужо ведае адказ на большасць пытанняў.
+        </p>
+        <p class="text-xs text-neutral-500 dark:text-neutral-300">
+
+        </p>
+      </div>
+    </div>
+    <div
+        class="flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700  md:flex-row">
+      <img  style="max-width: 200px"
+          class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+          src="../../assets/img/cheerful-pretty-businesswoman-near-business-plan.jpg"
+          alt="" />
+      <div class="flex flex-col justify-start p-6">
+        <h5
+            class="mb-2 text-xl font-medium text-red-700 dark:text-neutral-50">
+            Толькі рэальныя прыклады
+        </h5>
+        <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+          Дзеянні павінны быць асэнсаванымі, - нічога так не забівае матывацыю, як недарэчнае пачуццё бессэнсоўнасці і
+          бескарыснасці задачы. Прама як прыклады са школьных падручнікаў. Ніхто, акрамя таго, хто іх пісаў, не ведае,
+          навошта яны патрэбныя. Усе нашы задачы маюць сэнс і практычнае прымяненне.
+        </p>
+        <p class="text-xs text-neutral-500 dark:text-neutral-300">
+
+        </p>
+      </div>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
